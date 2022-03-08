d3b746a53f8baafd826413aa65066dd6c684f428
hill0ne <jeanj531@gmail.com>
2022-03-08 09:20:35 +0900

    [수정] 이벤트 위임을 사용한 코드개선 리펙토링

--- client/js/index.js
+++ client/js/index.js
@@ -23,54 +23,32 @@ function onAdd() {
 
   const item = createItem(text);
   items.appendChild(item);
-
+  item.scrollIntoView({ block: "center" });
   input.value = "";
   input.focus();
 }
 
+let id = 0;
+
 const createItem = function (text) {
   const itemRow = document.createElement("li");
   itemRow.setAttribute("class", "item__row");
+  itemRow.setAttribute("data-id", id);
 
-  const item = document.createElement("div");
-  item.setAttribute("class", "item");
+  itemRow.innerHTML = `
+  <div class="item">
+    <span class="item__content">${text}</span>
+      <div class="button__container">
+      <button class="item__delete">
+          <i class="fa fa-light fa-trash-can" id="trash" data-id=${id}></i>
+      </button>
+      <button class="item__special">
+          <i class="fa fa-light fa-star" id="star" data-id=${id}></i>
+      </button>
+    </div>
+</div>`;
 
-  const content = document.createElement("span");
-  content.setAttribute("class", "item__content");
-
-  const contentText = document.createTextNode(text);
-  content.appendChild(contentText);
-
-  // button
-  const ButnContainer = document.createElement("div");
-  ButnContainer.setAttribute("class", "button__container");
-  const delBtn = document.createElement("button");
-  delBtn.setAttribute("class", "item__delete");
-  delBtn.innerHTML = `<i class="fa fa-light fa-trash-can"></i>`;
-  delBtn.addEventListener("click", () => {
-    items.removeChild(itemRow);
-  });
-
-  const specialBtn = document.createElement("button");
-  specialBtn.setAttribute("class", "item__special");
-  specialBtn.innerHTML = `<i class="fa fa-light fa-star"></i>`;
-  specialBtn.addEventListener("click", () => {
-    items.prepend(itemRow);
-    itemRow.style.cssText = "color: red; font-weight: bold";
-  });
-
-  ButnContainer.appendChild(delBtn);
-  ButnContainer.appendChild(specialBtn);
-
-  //   const divider = document.querySelector("div");
-  //   divider.setAttribute("class", "divider");
-
-  item.appendChild(content);
-  item.appendChild(ButnContainer);
-
-  itemRow.appendChild(item);
-  //   itemRow.appendChild(divider);
-
+  id++;
   return itemRow;
 };
 
@@ -78,5 +56,17 @@ footerBtn.addEventListener("click", onAdd);
 input.addEventListener("keyup", (e) => {
   if (e.key === "Enter") {
     onAdd();
+  }
+});
+
+items.addEventListener("click", (e) => {
+  const id = e.target.dataset.id;
+  const selected = document.querySelector(`.item__row[data-id="${id}"]`);
+  console.log(selected);
+  if (e.target.id === "trash") {
+    selected.remove();
+  } else if (e.target.id === "star") {
+    items.prepend(selected);
+    selected.style.cssText = "color: #D82148; font-weight: bold;";
   }
 });
